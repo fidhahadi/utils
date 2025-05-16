cdafe64c1804ee9c816cf55eb2ca5d3a8cb14b2f
Fidha Rafeeque <fidhahadi@gmail.com>
2025-05-16 02:53:23 -0700

    Added setup.py

---
+++ ml_utils/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ml_utils",  # <== THIS NAME MUST MATCH #egg=ml_utils
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+)
